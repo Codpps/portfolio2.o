d75ca54e4d9af5c861275b374e7c0db5a34ab4c9
Codpps <fairuzdafa85@gmail.com>
2025-02-27 11:23:47 +0700

    Resolve merge conflicts

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'index'])->name('index');
 Route::get('/workShow/{id}', [PageController::class, 'show'])->name('workshow');
-Route::resource('/experience', ExperienceController::class);
-Route::resource('/company', CompanyController::class);
-Route::resource('/galery', GaleryController::class);
-Route::resource('/contact', ContactController::class);
-Route::resource('/tech', TechController::class);
-Route::resource('/work', WorkController::class);
